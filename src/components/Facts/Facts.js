cf48b7737336772755d68e3ce8ced4fd009e2efc
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2021-12-12 02:37:52 +0530

    Facts Component

--- src/components/Facts/Facts.js
+++ src/components/Facts/Facts.js
@@ -1,8 +1,56 @@
 import './Facts.css';
+import {FACTS} from '../../shared/facts';
+
+const IMG = (imgName) => {
+  return require(`../../images/${imgName}`);
+}
+
+const Fact = ({index, item}) => {
+
+  const imageSrc = (IMG(item.image) || {}).default;
+  return (
+    <div className={"fact " + (index % 2 === 0 ? '' : 'fact--grey')}>
+      <div className={"container fact__container " + (index % 2 === 0 ? 'fact__container--normal' : '')}>
+
+        <div className="fact__image">
+          <img src={imageSrc} alt={item.alt} width="100%" height="auto" />
+        </div>
+        
+        <div className="fact__details">
+          <h3> {item.title} </h3>
+          <p> {item.description} </p>
+          <button>Buy Subscription</button>
+        </div>
+      
+      </div>
+    </div>
+  )
+}
+
+const FactsList = () => {
+  const factsList = FACTS || [];
+  return (
+    <>
+    {
+      factsList.map((fact, index) => (
+        <Fact key={fact.id} index={index} item={fact} />
+      ))
+    }
+    </>
+  )
+}
 
 const Facts = () => {
   return (
-    <div>Facts</div>
+    <div className="facts">
+      <header>GET STARTED TODAY</header>
+      <h2>
+        Smarter Digitization, 
+        <br />
+        Leads to Better Immunization
+      </h2>
+      <FactsList />
+    </div>
   )
 }
 
---
+++ src/shared/facts.js
@@ -0,0 +1,37 @@
+export const FACTS = [
+  {
+    id: 0,
+    title: 'Digitized Vaccination records and Reminders',
+    image: 'features01.png',
+    alt: "Vaccination reminders",
+    description: "Vaccination records are your baby’s first and one of the most crucial health records. ImmunifyMe keeps these records digital, so you do not lose them ever. If you are still using the old paper booklet to keep your child’s vaccination records, find out why you should switch to ImmunifyMe now!"
+  },
+  {
+    id: 1,
+    title: 'Telemedicine 2.0',
+    image: 'features02.png',
+    alt: "Telemedicine",
+    description: "Have an urgent meeting, but also need to see a pediatrician? No need to take a day off. Consult with your favourite pediatrician from the comfort of your living room, or even book an in-clinic appointment on your preferred date and time!"
+  },
+  {
+    id: 2,
+    title: 'Digital Prescription',
+    image: 'features03.png',
+    alt: "Digital Prescription",
+    description: "All the written prescriptions can be stored and shared whenever needed. So, no more hassles in finding the paper prescription. Store all the health records of your child in one place, never to lose it again!"
+  },
+  {
+    id: 3,
+    title: 'Milestone Tracking',
+    image: 'features04.png',
+    alt: "Milestone Tracking",
+    description: "The first few years are very crucial for your child’s growth. Our intelligent AI-powered programming assesses your child’s growth on various parameters like physical, mental, emotional and cognitive, etc. It predicts the onset of certain anomalies, thus, giving you an early warning to consult the specialists on time!"
+  },
+  {
+    id: 4,
+    title: 'Customized Nutrition Plan',
+    image: 'features05.png',
+    alt: "Customized Nutrition Plan",
+    description: "It is said that you become what you eat! The first two years are important for your child’s growth and parents often worry about what to feed their child. With the help of one-on-one teleconsultation and our AI-powered recommendation tool, our team of professional nutritionists with specialisation in child nutrition help you choose the right nutrition for your child. All these and more for less than Rs. 10 a day!"
+  }
+]
