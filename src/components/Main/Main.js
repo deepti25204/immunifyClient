741b3b471e284e69a4b37f6d4b9d5abb068607fe
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2021-12-12 02:39:51 +0530

    Plan Component- update

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,7 @@ import './Main.css';
 import Banner from '../Banner/Banner';
 import Facts from '../Facts/Facts';
 import Pitch from '../Pitch/Pitch';
-import Plan from '../Plan/Plan';
+import Plans from '../Plans/Plans';
 import Testimonials from '../Testimonials/Testimonials';
 import Awards from '../Awards/Awards';
 import Wordpress from '../Wordpress/Wordpess';
@@ -14,7 +14,7 @@ const Main = () => {
       <Banner />
       <Facts />
       <Pitch />
-      <Plan />
+      <Plans />
       <Testimonials />
       <Awards />
       <Wordpress />
