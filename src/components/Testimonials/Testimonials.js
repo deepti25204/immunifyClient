b22605e2c3a45cdba9ef3e9600b24a8362555733
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2021-12-12 04:44:11 +0530

    Testimonials component

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,8 +1,67 @@
 import './Testimonials.css';
+import quoteLeft from '../../images/quote-sign-left.png';
+import quoteRight from '../../images/quote-sign-right.png';
+
+import { TESTIMONIALS } from '../../shared/testimonials';
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
+import SwiperCore, {  Pagination, Autoplay } from 'swiper';
+
+import "swiper/swiper.min.css";
+import "swiper/modules/pagination/pagination.min.css";
+
+SwiperCore.use([Autoplay, Pagination]);
+
+const IMG = (imgName) => {
+  return require(`../../images/testimonial/${imgName}`);
+}
 
 const Testimonials = () => {
+  const slides = [];
+
+  TESTIMONIALS.map((item, index) => {
+    const imageSrc = (IMG(item.image) || {}).default;
+    return (
+    slides.push(
+      <SwiperSlide key={item.id}>
+        <div className="testimonial">
+          <div className="testimonial__image">
+            <img src={imageSrc} alt={item.author} width="75px" height="75px" />
+          </div>
+
+          <div className='testimonial__details'>
+            {index % 2 === 0 ? < br/> : ''}
+            <img src={quoteLeft} alt="quote left"/>
+            <span>{item.quote}</span>
+            <img src={quoteRight} alt="quote right"/>
+            <footer className="testimonial__author">
+              <cite title="Source Title">{item.author}</cite>
+            </footer>
+          </div>
+        </div>
+      </SwiperSlide>
+    ))
+  });
+
   return (
-    <div>Testimonials</div>
+    <section className="testimonials">
+      <h2>Testimonials</h2>
+      <div className="testimonials__subheading">
+        comments we got from parents around the world!
+      </div>
+
+      <div className="testimonials__swipercontainer">
+        <Swiper className="testimonials__swiper"
+          pagination={{ clickable: true}}
+          autoplay={{ delay: 8000 }}
+          spaceBetween={30}
+          slidesPerView={1}
+          allowTouchMove={true}
+          >
+            {slides}
+        </Swiper>
+      </div>
+
+    </section>
   )
 }
 
---
+++ src/shared/testimonials.js
@@ -0,0 +1,26 @@
+export const TESTIMONIALS = [
+  {
+    id: 'testimonial1',
+    image: 'kiran.jpg',
+    quote: "Great initiative☺️, Nowadays digitalisation is very important and needed, it will be very helpful for all of us♥️",
+    author: 'Kiran Sharma'
+  },
+  {
+    id: 'testimonial2',
+    image: 'harsh.jpg',
+    quote: "I really like the concept of digitizing the immunization record ... It's Not only helpfull for parents but it'll also help the doctors to monitor child's vaccination records very easily.",
+    author: 'Harsh Singhal'
+  },
+  {
+    id: 'testimonial3',
+    image: 'upendra.jpg',
+    quote: "I really liked Immunifyme, Its one of the first tool for digitizing immunization. I definitely recomnmend young parents to go for it.",
+    author: 'Upendra Kumar Sharma'
+  },
+  {
+    id: 'testimonial4',
+    image: 'priyanka.jpg',
+    quote: "Immunifyme making vaccination process easier ....now there is no need to do paper works, everything is digitalized. It is also very easy to use and I found it very useful for all.😊",
+    author: 'Priyanka Sharma'
+  }
+]
