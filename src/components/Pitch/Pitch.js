46c4874809ca40eb21f59347ea41387f7abd2af8
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2021-12-12 02:38:30 +0530

    Pitch Component

--- src/components/Pitch/Pitch.js
+++ src/components/Pitch/Pitch.js
@@ -1,8 +1,20 @@
 import './Pitch.css';
+import pitch from '../../images/pitch.svg';
 
 const Pitch = () => {
   return (
-    <div>Pitch</div>
+    <div className="pitch pitch--purplelight">
+      <div className="container pitch__container">
+        <div className="pitch__image">
+          <img src={pitch} alt="Data Security Circle" width="100%" height="auto" />
+        </div>
+
+        <div className="pitch__details">
+          <h3>Your Private Data Remains Private</h3>
+          <p> Your health files always remain encrypted with us and we never use it in any external promotions. Be reassured that we are both GDPR and HIPAA compliant platform, if that satisfies your curious soul. </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
