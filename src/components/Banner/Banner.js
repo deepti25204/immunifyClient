1d286020852f0c644d649628380bb809ff7cd690
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2021-12-12 20:55:48 +0530

    Minor CSS changes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -12,20 +12,22 @@ const Banner = () => {
         </div>
         
         <div className="banner__description">
-          <h1 className="banner__heading">
-            Give your child the gift of 
-            "<span className="banner__heading--highlight">Good Health</span>"with
-            <img className="banner__imagelogo" src={bannerlogo} alt="Immunifyme Logo" width="70%" height="auto" />
-          </h1>
+          <div>
+            <h1 className="banner__heading">
+              Give your child the gift of 
+              "<span className="banner__heading--highlight">Good Health</span>"with
+              <img className="banner__imagelogo" src={bannerlogo} alt="Immunifyme Logo" width="70%" height="auto" />
+            </h1>
 
-          <div className="banner__linkbuttons">
-            <div className="banner__subscription">
-              <button>Subscribe Now</button>
-            </div>
-            <div className="banner__androidbtn">
-              <a href="https://play.google.com/store/apps/details?id=com.immparentportal&hl=en&gl=US" >
-                <img src={googlePlay} alt="Google Play Icon" width="135px" height="40px" />
-              </a>
+            <div className="banner__linkbuttons">
+              <div className="banner__subscription">
+                <button>Subscribe Now</button>
+              </div>
+              <div className="banner__androidbtn">
+                <a href="https://play.google.com/store/apps/details?id=com.immparentportal&hl=en&gl=US" >
+                  <img src={googlePlay} alt="Google Play Icon" width="135px" height="40px" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
