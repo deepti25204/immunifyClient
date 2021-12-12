b682992153f78346fafb18d18b3f754c51f87706
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2021-12-12 17:14:23 +0530

    Awards Component

--- src/components/Awards/Awards.js
+++ src/components/Awards/Awards.js
@@ -1,8 +1,59 @@
 import './Awards.css';
+import { AWARDS } from '../../shared/awards';
+
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
+import SwiperCore, { Autoplay, Pagination } from 'swiper';
+import "swiper/swiper.min.css";
+SwiperCore.use([Autoplay, Pagination]);
+
+const IMG = (imgName) => {
+  return require(`../../images/awards/${imgName}`);
+} 
+
+const Award = ({item}) => {
+  const imageSrc = (IMG(item.image) || {}).default;
+  return (
+    <div className="awards_image">
+      <img src={imageSrc} alt={item.alt} width="100%" height="auto" />
+    </div>
+  )
+}
 
 const Awards = () => {
+  const slides = [];
+
+  AWARDS.map((awardList, index) => {
+    return (
+      slides.push(
+        <SwiperSlide key={"awardList" +index} className="awards__items">
+          <div className="awards_images">
+            {awardList.map(item => <Award key={item.id} item={item} />)}
+          </div>
+        </SwiperSlide>
+      )
+    )
+  })
   return (
-    <div>Awards</div>
+    <section className="awards">
+      <h2>Awards & Grants</h2>
+      <div className="awards__content">
+        <div className="container">
+          <div className="awards__row">
+            <Swiper className="awards__swiper"
+            pagination={{ clickable: true}}
+            autoplay={{ delay: 8000 }}
+            spaceBetween={50}
+            slidesPerView={1}
+            allowTouchMove={false}
+            loop={true}
+            speed={600}
+            >
+              {slides}
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/shared/awards.js
@@ -0,0 +1,55 @@
+export const AWARDS = [
+  [
+    {
+      id: 'awards1',
+      image: 'infuseLogo.png',
+      content: "ImmunifyMe became a member of GAVI INFUSE and also declared as the Best 10 Startups working on Immunization",
+      alt: 'Infuse'
+    },
+    {
+      id: 'awards2',
+      image: 'luxinnovationLogo.png',
+      content: "ImmunifyMe received Commonwealth Secretary Generals Innovation for Sustainable development award",
+      alt: 'Luxinnovation'
+    },
+    {
+      id: 'awards3',
+      image: 'whsLogo.png',
+      content: "ImmunifyMe was declared as the Top10 Startups in Healthcare and Healthcare Innovations",
+      alt: 'World Health Summit'
+    },
+    {
+      id: 'awards4',
+      image: 'fit4Start.png',
+      content: "ImmunifyMe received the Grant worth 2,00,000 USD. Also declared as the 5 Best Healthcare Innovation",
+      alt: 'Fit 4 Start'
+    }
+  ],
+  [
+    {
+      id: 'awards5',
+      image: 'ifcLogo.png',
+      content: "ImmunifyMe was one of the finalists in the Transformational Business Award",
+      alt: 'International Financial Corporation'
+    },
+    {
+      id: 'awards6',
+      image: 'beneluxLogo.png',
+      content: "ImmunifyMe awarded as the Best Innovation award for immunization in Europe Region",
+      alt: 'Benelux Business'
+    },
+    {
+      id: 'awards7',
+      image: 'startupThailand.png',
+      content: "ImmunifyMe was declared as the TOP GLOBAL IMPACT PERFORMER in 2017 at StartUp Thailand",
+      alt: 'Startup Thailand'
+    },
+    {
+      id: 'awards8',
+      image: 'startupChilli.png',
+      content: "ImmunifyMe was declared as the TOP GLOBAL IMPACT PERFORMER in 2017 at StartUp Thailand",
+      alt: 'Startup Chilli'
+    }
+
+  ]
+]
