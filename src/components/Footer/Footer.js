3db9cc3a3da3466fba57128f395a7a443d86449a
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2021-12-12 21:59:49 +0530

    Footer Component - Initial (Keywords)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,29 @@
 import './Footer.css';
+import { SEARCH_KEYWORDS } from '../../shared/searchkeywords';
+
+const Keyword = ({item, last}) => {
+  return (
+    <>
+      <a href={item.link}>{item.title}</a>
+      {last ? '' : ' | '}
+    </>
+  )
+}
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <div className="container footer__container">
+        <div className="footer__keywords">
+          <h4>Popular Searches</h4>
+          <p>
+            {SEARCH_KEYWORDS.map((item, index) => (
+              <Keyword key={item.id} item={item} last={index===(SEARCH_KEYWORDS.length -1 )} />)
+            )}
+          </p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
---
+++ src/shared/searchkeywords.js
@@ -0,0 +1,97 @@
+export const SEARCH_KEYWORDS = [
+  {
+    id: 'keyword1',
+    title: 'Child Vaccination in India',
+    link: 'https://blog.immunifyme.com/hpv-you-would-do-anything-to-protect-your-child-from-cancer/',
+  },
+  {
+    id: 'keyword2',
+    title: 'Child Immunization in India',
+    link: 'https://blog.immunifyme.com/routine-childhood-immunization-in-times-of-coronavirus-lockdown/',
+  },
+  {
+    id: 'keyword3',
+    title: 'Digital Vaccination Records',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword4',
+    title: 'Child Vaccination Records',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword5',
+    title: 'Immunization Records Online',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword6',
+    title: 'Vaccination Reminder App',
+    link: 'https://immunifyme.com/parent/aboutUs',
+  },
+  {
+    id: 'keyword7',
+    title: 'How to Get Your Immunization Records Online',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword8',
+    title: 'What is a Child Immunization Record',
+    link: 'https://blog.immunifyme.com/routine-childhood-immunization-in-times-of-coronavirus-lockdown/',
+  },
+  {
+    id: 'keyword9',
+    title: 'What are the 10 most Important Vaccines',
+    link: 'https://blog.immunifyme.com/vaccination-understanding-its-underrated-importance-in-our-lives-2/',
+  },
+  {
+    id: 'keyword10',
+    title: 'What Vaccines are Absolutely Necessary',
+    link: 'https://blog.immunifyme.com/vaccination-understanding-its-underrated-importance-in-our-lives-2/',
+  },
+  {
+    id: 'keyword11',
+    title: 'Baby Immunization Schedule',
+    link: 'https://blog.immunifyme.com/routine-childhood-immunization-in-times-of-coronavirus-lockdown/',
+  },
+  {
+    id: 'keyword12',
+    title: 'Government Vaccination Schedule in India',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword13',
+    title: 'Vaccination for Babies in India',
+    link: 'https://blog.immunifyme.com/vaccine-education-is-a-must-in-both-the-rural-and-urban-populations/',
+  },
+  {
+    id: 'keyword14',
+    title: 'New Born Baby Vaccination in India',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword15',
+    title: 'National Immunization Schedule',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword16',
+    title: 'IAP Immunization Schedule',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword17',
+    title: 'Vaccine Schedule in India',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword18',
+    title: 'Latest Immunization Schedule in India',
+    link: 'https://blog.immunifyme.com/vaccination-schedule-for-2020-21-iap-recommended/',
+  },
+  {
+    id: 'keyword19',
+    title: 'Vaccine Preventable Diseases in India',
+    link: 'https://blog.immunifyme.com/fighting-viruses-with-vaccines/',
+  }
+]
