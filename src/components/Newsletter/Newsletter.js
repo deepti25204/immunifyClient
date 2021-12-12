dbf1e3e9efde15dc45a22bdce2b6dd863de155fb
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2021-12-12 20:17:25 +0530

    Newsletter Component- Initial

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,8 +1,35 @@
 import './Newsletter.css';
 
 const Newsletter = () => {
+
+  const subscribe = () => {
+    // validation
+    // call api
+    console.log("subscribe");
+  }
+
   return (
-    <div>Newsletter</div>
+    <section className="newsletter">
+      <div className="newsletter__container container">
+        <p>
+          Keep the Moms and Children in your family
+          healthy. Subscribe to our weekly newsletter now! 
+        </p>
+        <div className="newsletter__inputgroup">
+          <div className="newsletter__inputs">
+            <input type="email" pattern=".+@globex\.com" size="30" required 
+              className="newsletter__email" 
+              placeholder="Email here" 
+              aria-label="Email here" aria-describedby="button-addon2" id="emailInputBottom" />
+            <div className="newsletter__btn">
+              <button type="button" id="subscribeBtn" onClick={subscribe()}>
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
