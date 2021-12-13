c5cd547cb6eff9676fead283f82a108b106541f1
Deepti Aggarwal <er.deepti.aggarwal@outlook.com>
2021-12-13 07:48:02 +0530

    header Component - Initial

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,29 @@
 import './Header.css';
+import ImmunifyMelogo from '../../images/ImmunifyMelogo.svg';
+import breadcrumb from '../../images/breadcrumb.svg';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <nav className="container">
+        <a className="header__brand" href="https://immunifyme.com/">
+          <img src={ImmunifyMelogo} alt="Immunify Logo" width="80%" height="auto" />
+        </a>
+        <div className="header_right">
+          <div className="header__signin">
+            <a role="button" href="https://admin.immunifyme.com/" target="_blank" rel="noreferrer">
+              Sign in
+            </a>
+          </div>
+          <div className="header__breadcrumb">
+            <button type="button">
+              <span style={{backgroundImage: `url(${breadcrumb})`}}></span>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+    
   )
 }
 
